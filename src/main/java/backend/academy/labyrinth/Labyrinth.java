9b52f2c20d5ae8dfc78aea92d0355bc0d0cd6bcf
6ocra3 <6o3kmm@mail.ru>
2024-10-09 23:21:18 +0300

    feat: add HuntAndKill Generator

--- src/main/java/backend/academy/labyrinth/Labyrinth.java
+++ src/main/java/backend/academy/labyrinth/Labyrinth.java
@@ -1,6 +1,7 @@
 package backend.academy.labyrinth;
 
 import backend.academy.labyrinth.extraStructures.point.Point;
+import backend.academy.labyrinth.generators.HuntAndKillGenerator;
 import backend.academy.labyrinth.generators.KraskalGenerator;
 import backend.academy.labyrinth.inputOutput.DefaultIO;
 import backend.academy.labyrinth.maze.Maze;
@@ -12,24 +13,25 @@ public class Labyrinth {
         System.out.println("Labyrinth запущен");
         int width = 10;
         int height = 10;
-        KraskalGenerator generator = new KraskalGenerator(width, height);
+//        KraskalGenerator generator = new KraskalGenerator(width, height);
+        HuntAndKillGenerator generator = new HuntAndKillGenerator(width, height);
         generator.generate();
         Point start = new Point(0, 0);
         Point end = new Point(width-1, height-1);
         Maze maze = new Maze(generator.maze(),width, height, start, end);
         DefaultIO defaultIO = new DefaultIO();
         defaultIO.visualizeMaze(maze);
-        DFSSolver dfsSolver = new DFSSolver();
-        dfsSolver.maze(maze);
-        Maze solvedMaze = dfsSolver.stepByStepSolve();
-        while (!dfsSolver.isFinished()){
-            dfsSolver.nextStep();
-            defaultIO.visualizeMaze(solvedMaze);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+//        DFSSolver dfsSolver = new DFSSolver();
+//        dfsSolver.maze(maze);
+//        Maze solvedMaze = dfsSolver.stepByStepSolve();
+//        while (!dfsSolver.isFinished()){
+//            dfsSolver.nextStep();
+//            defaultIO.visualizeMaze(solvedMaze);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
---
+++ src/main/java/backend/academy/labyrinth/generators/HuntAndKillGenerator.java
@@ -0,0 +1,101 @@
+package backend.academy.labyrinth.generators;
+
+import backend.academy.labyrinth.extraStructures.edge.Edge;
+import backend.academy.labyrinth.extraStructures.point.Point;
+import lombok.Getter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class HuntAndKillGenerator {
+    int width;
+    int height;
+    @Getter
+    List<Edge> maze = new ArrayList<>();
+    boolean[][] visited;
+    Random rnd = new Random();
+
+
+
+    public HuntAndKillGenerator(int width, int height){
+        this.width = width;
+        this.height = height;
+        visited = new boolean[height][width];
+        for(int i = 0; i<height;i++){
+            Arrays.fill(visited[i], false);
+        }
+    }
+
+    public boolean inBounds(int x, int y){
+        return x >= 0 && y >= 0 && x < width && y < height;
+    }
+
+    public List<Point> findNeighbours(Point p, boolean visitedValue){
+        List<Point> neighbours = new ArrayList<>();
+        int[][] dxdy = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        for(int[] temp : dxdy){
+            int nx = p.x() + temp[0];
+            int ny = p.y() + temp[1];
+            if(inBounds(nx, ny) && visited[ny][nx] == visitedValue){
+                neighbours.add(new Point(ny, nx));
+            }
+        }
+        return neighbours;
+    }
+
+    public void kill(Point p){
+        visited[p.y()][p.x()] = true;
+
+        while (true){
+            List<Point> neighbours = findNeighbours(p, false);
+
+            if(neighbours.isEmpty()){
+                break;
+            }
+
+            Point next = neighbours.get(rnd.nextInt(neighbours.size()));
+
+            Edge edge = new Edge(p, next);
+            maze.add(edge);
+            visited[next.y()][next.x()] = true;
+
+            p = next;
+        }
+    }
+
+    public Point hunt(){
+        for(int y = 0; y<height;y++){
+            for(int x = 0; x<width;x++){
+                if(!visited[y][x]){
+                    Point cur = new Point(y, x);
+                    List<Point> neighbours = findNeighbours(cur, true);
+                    if(!neighbours.isEmpty()){
+                        Point prev = neighbours.get(rnd.nextInt(neighbours.size()));
+                        visited[y][x] = true;
+                        maze.add(new Edge(prev, cur));
+                        return cur;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public void generate(){
+        int startX = rnd.nextInt(width);
+        int startY = rnd.nextInt(height);
+
+        kill(new Point(startY, startX));
+        while (true){
+            Point p = hunt();
+            if(p == null){
+                break;
+            }
+            kill(p);
+        }
+    }
+}
